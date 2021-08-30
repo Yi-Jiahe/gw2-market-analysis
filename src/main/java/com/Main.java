0316c1efa528a78137b9f91d99e40a7a1a442531
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-08-31 00:04:05 +0800

    start on price getter function

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,17 +1,22 @@
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
+import java.util.Map;
+
+import com.ParameterStringBuilder;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.println(Arrays.toString(getItemList()));
+            String[] itemids = getItemIdList();
+
+            System.out.println(String.join(",", itemids));
         } catch (Exception e) {
 
         }
     }
 
-    static String[] getItemList() throws Exception {
+    static String[] getItemIdList() throws Exception {
         URL url = new URL("https://api.guildwars2.com/v2/commerce/prices/");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -38,5 +43,20 @@ public class Main {
         con.disconnect();
 
         return itemids;
+    }
+
+    static void getPrices(String[] itemIds) {
+        URL url = new URL("https://api.guildwars2.com/v2/commerce/prices/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("ids", String.join(",", itemIds));
+
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+        out.flush();
+        out.close();
     }
 }
