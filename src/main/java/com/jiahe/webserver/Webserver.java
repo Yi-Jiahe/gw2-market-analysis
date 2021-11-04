ab874abc3ed059a6416492dd584e35b70f1e39b0
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-11-04 11:36:53 +0800

    Implement static file serving with thymeleaf

--- src/main/java/com/jiahe/webserver/Webserver.java
+++ src/main/java/com/jiahe/webserver/Webserver.java
@@ -1,15 +1,21 @@
 package com.jiahe.webserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Webserver {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/index")
+    public String serveIndex(Model model) {
+        return "index";
     }
 }
